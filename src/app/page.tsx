87a251825d3a2b3f4e0c33ffd7fae775fa91c0da
francismiko <1485059943@qq.com>
2024-03-09 23:36:44 +0800

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import { Empty } from "@douyinfe/semi-ui";
 import React from "react";
+import { IllustrationIdle } from "@douyinfe/semi-illustrations";
 
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="flex justify-center items-center h-full">
+			<Empty
+				image={<IllustrationIdle style={{ width: 300, height: 300 }} />}
+				description={"这是一个AI 英语自学平台"}
+			/>
+		</div>
+	);
 }
