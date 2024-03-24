7cebc5ae7a7b9d4b362c277f9f5603034b66b4a9
francismiko <1485059943@qq.com>
2024-03-24 15:37:10 +0800

    feat: message delete api

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -26,3 +26,29 @@ export async function GET(req: NextRequest) {
 		return Response.json({ status: "error", message: e.message });
 	}
 }
+
+export async function DELETE(req: NextRequest) {
+	const searchParams = req.nextUrl.searchParams;
+	const owner = searchParams.get("owner");
+
+	try {
+		if (owner) {
+			const chat = await prisma.chat.findFirst({
+				where: {
+					owner,
+				},
+			});
+
+			const messages = await prisma.message.deleteMany({
+				where: {
+					chatId: chat?.id,
+				},
+			});
+
+			return Response.json(messages);
+		}
+	} catch (e: any) {
+		console.log(e);
+		return Response.json({ status: "error", message: e.message });
+	}
+}
