9715fc3f702e0006926cb61ec104e33195fa18cf
francismiko <1485059943@qq.com>
2024-04-01 16:24:10 +0800

    feat: parse mock exam

--- src/app/mock-exam/page.tsx
+++ src/app/mock-exam/page.tsx
@@ -8,6 +8,7 @@ import {
 	Empty,
 	Spin,
 	TextArea,
+	Notification,
 } from "@douyinfe/semi-ui";
 import { IconPlus } from "@douyinfe/semi-icons";
 import { IllustrationSuccess } from "@douyinfe/semi-illustrations";
@@ -24,7 +25,14 @@ export default function MockExam() {
 	const [readingPart3, setReadingPart3] = useState<string>("");
 	const [translation, setTranslation] = useState<string>("");
 	const [writing, setWriting] = useState<string>("");
-	const [textAreaValues, setTextAreaValues] = useState<string[]>([]);
+	const [textAreaValues, setTextAreaValues] = useState<string[]>([
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+	]);
 	const examContent = [
 		select,
 		readingPart1,
@@ -33,7 +41,39 @@ export default function MockExam() {
 		translation,
 		writing,
 	];
+	const [parseSelect, setParseSelect] = useState<string>("");
+	const [parseReadingPart1, setParseReadingPart1] = useState<string>("");
+	const [parseReadingPart2, setParseReadingPart2] = useState<string>("");
+	const [parseReadingPart3, setParseReadingPart3] = useState<string>("");
+	const [parseTranslation, setParseTranslation] = useState<string>("");
+	const [parseWriting, setParseWriting] = useState<string>("");
+	const parseExamContent = [
+		parseSelect,
+		parseReadingPart1,
+		parseReadingPart2,
+		parseReadingPart3,
+		parseTranslation,
+		parseWriting,
+	];
+	const setParseExamContent = [
+		setParseSelect,
+		setParseReadingPart1,
+		setParseReadingPart2,
+		setParseReadingPart3,
+		setParseTranslation,
+		setParseWriting,
+	];
 	const [isEmpty, setIsEmpty] = useState<boolean>(true);
+	const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+	const handleStream = async (
+		stream: AsyncIterable<string>,
+		setState: React.Dispatch<React.SetStateAction<string>>,
+	) => {
+		for await (const chunk of stream) {
+			setState((prev) => prev + chunk);
+		}
+	};
 
 	const handleGenerateExam = async () => {
 		const stream = async (prompt: string) => {
@@ -47,15 +87,6 @@ export default function MockExam() {
 				{ baseURL: process.env.proxyUrl },
 			);
 			return model.pipe(parser).stream(prompt);
-		};
-
-		const handleStream = async (
-			stream: AsyncIterable<string>,
-			setState: React.Dispatch<React.SetStateAction<string>>,
-		) => {
-			for await (const chunk of stream) {
-				setState((prev) => prev + chunk);
-			}
 		};
 
 		const outputFromat =
@@ -74,19 +105,19 @@ export default function MockExam() {
 			),
 			handleStream(
 				await stream(
-					`${systemFromat}模拟CET-6难度的阅读理解题目, 只需要你生成其中的第一篇即可, 题材是关于商业新闻的, 控制阅读时间在10-15分钟至少700字, 至少有3个选择题目, 难度为中等, 前面放个大标题'阅读练习 (3 passages, 30 points)', ${outputFromat}`,
+					`${systemFromat}模拟CET-6难度的阅读理解选择题目, 只需要你生成其中的第一篇即可, 题材是关于商业新闻的, 控制阅读时间在10-15分钟至少700字, 至少有3个选择题目, 难度为中等, 前面放个大标题'阅读练习 (3 passages, 30 points)', ${outputFromat}`,
 				),
 				setReadingPart1,
 			),
 			handleStream(
 				await stream(
-					`${systemFromat}模拟CET-6难度的阅读理解题目, 只需要你生成其中的第二篇即可, 题材是关于科技环境的, 控制阅读时间在10-15分钟至少700字, 至少有3个选择题目, 难度为中等, 前面放个标题'Passage 2', ${outputFromat}`,
+					`${systemFromat}模拟CET-6难度的阅读理解选择题目, 只需要你生成其中的第二篇即可, 题材是关于科技环境的, 控制阅读时间在10-15分钟至少700字, 至少有3个选择题目, 难度为中等, 前面放个标题'Passage 2', ${outputFromat}`,
 				),
 				setReadingPart2,
 			),
 			handleStream(
 				await stream(
-					`${systemFromat}模拟CET-6难度的阅读理解题目, 只需要你生成其中的第三篇即可, 题材是关于人文故事的, 控制阅读时间在15-20分钟至少900字, 至少有3个选择题目, 难度为困难, 前面放个标题'Passage 3', ${outputFromat}`,
+					`${systemFromat}模拟CET-6难度的阅读理解选择题目, 只需要你生成其中的第三篇即可, 题材是关于人文故事的, 控制阅读时间在15-20分钟至少900字, 至少有3个选择题目, 难度为困难, 前面放个标题'Passage 3', ${outputFromat}`,
 				),
 				setReadingPart3,
 			),
@@ -113,8 +144,38 @@ export default function MockExam() {
 		});
 	};
 
-	const handleSubmitExam = async (answer: string[]) => {
-		console.log(answer);
+	const handleSubmitExam = async (answers: string[]) => {
+		if (textAreaValues.some((value) => value === "")) {
+			Notification.error({
+				title: "还有没回答的题目!",
+				duration: 0.8,
+			});
+			return;
+		}
+
+		const model = new OpenAI(
+			{
+				modelName: "gpt-3.5-turbo",
+				openAIApiKey: process.env.openAIApiKey,
+				temperature: 0,
+			},
+			{ baseURL: process.env.proxyUrl },
+		);
+
+		answers.forEach(async (answer, index) => {
+			const prompt = `这是我给出的题目:
+			'''
+			${examContent[index]}
+			'''
+			然后这是用户给出的答案:
+			'''
+			${answer}
+			'''
+			请你作为阅卷人来评判用户的答案, 并给出正确的答案和答案解析.
+			`;
+			const stream = await model.stream(prompt);
+			handleStream(stream, setParseExamContent[index]);
+		});
 	};
 
 	return (
@@ -134,21 +195,27 @@ export default function MockExam() {
 						<>
 							<button
 								type="button"
-								className="cta fixed right-28 top-28"
+								className="cta fixed right-28 top-28 z-10"
 								onClick={() => handleSubmitExam(textAreaValues)}
 							>
-								<span className="hover-underline-animation">提交试卷</span>
+								<span className="hover-underline-animation font-medium italic">
+									提交试卷
+								</span>
 							</button>
 							{examContent.map((content, index) => (
 								<>
 									<Markdown>{content}</Markdown>
 									<h6>答题区：</h6>
 									<TextArea
+										showClear
 										placeholder="在此填入答案..."
 										value={textAreaValues[index]}
 										onChange={(value) => handleTextAreaChange(index, value)}
 										autosize
 									/>
+									<Markdown className="text-red-400">
+										{parseExamContent[index]}
+									</Markdown>
 									{index < examContent.length - 1 && <Divider margin="12px" />}
 								</>
 							))}
